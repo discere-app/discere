c6ccaee17c358678cff79d8083daf1eb3fb1bb39
Fabian Eberle <eberlefabian@gmail.com>
2026-02-27 19:15:54 +0100

    chore: Upgrade Flutter, update iOS platform to 13.0, adopt implicit engine delegate, and add mockito dependency.

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -2,13 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:discere/main.dart' as app;
+import 'package:discere/service/common/notification_service.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+@GenerateNiceMocks([MockSpec<NotificationService>()])
+import 'app_test.mocks.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('Smoke test: verify app starts and shows correct title', (WidgetTester tester) async {
-    // Start the app
-    await app.main();
+    final mockNotificationService = MockNotificationService();
+    when(mockNotificationService.initNotification()).thenAnswer((_) async {});
+    when(mockNotificationService.requestPermissions()).thenAnswer((_) async {});
+
+    // Start the app with a mock service that doesn't trigger native dialogs
+    await app.main(notificationService: mockNotificationService);
 
     // Give the app time to fully render the first frame and settle animations
     // The permission dialog might show up here depending on os versions,
