3cfa5b48257df979f346d414305701eb88c5b9b8
Fabian Eberle <eberlefabian@gmail.com>
2026-03-30 12:45:16 +0200

    feat: replace individual card notifications with a daily summary rescheduling system

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 
 import 'dart:io';
-import 'package:flutter/foundation.dart';
 import 'package:discere/persistence/deck_repository.dart';
 import 'package:discere/persistence/flash_card_stat_repository.dart';
 import 'package:discere/persistence/source_repository.dart';
@@ -84,9 +83,6 @@ Future<List<SingleChildWidget>> setupServices({NotificationService? notification
   final importExportService = ImportExportService(deckService, speciesRepository, imageService);
   final sourceService = SourceService(sourcesRepository);
 
-  if (kDebugMode) {
-    await deckService.createDummyDecks();
-  }
 
   return [
     Provider<ImageService>.value(value: imageService),
