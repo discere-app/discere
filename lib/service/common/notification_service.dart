56e1175065c3c5b1306ce76c4a53c5e5252d1d72
Fabian Eberle <eberlefabian@gmail.com>
2026-04-02 12:03:17 +0200

    change app name and fix release start

--- lib/service/common/notification_service.dart
+++ lib/service/common/notification_service.dart
@@ -14,7 +14,7 @@ class NotificationService {
 
   Future<void> initNotification() async {
     AndroidInitializationSettings initializationSettingsAndroid =
-        const AndroidInitializationSettings("@mipmap/ic_launcher");
+        const AndroidInitializationSettings("@mipmap/launcher_icon");
 
     var initializationSettingsIOS = const DarwinInitializationSettings(
       requestAlertPermission: true,
